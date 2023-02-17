5d5116dc9fe4a75b6a6ef8c1854a2bbd420488e5
g0thic <mustafa.x@Msn.com>
2023-02-18 00:38:18 +0200

    Update address_factory.py

--- address_factory.py
+++ address_factory.py
@@ -40,19 +40,7 @@ class Lib2(Address):
         self.address.clear()
         self.GetAddress1()
         self.GetAddress2()
-        # self.GetAddress3()
-
-    def GetAddress3(self):
-
-        for x in range(5):
-            setup('mainnet')
-            xx = random.randint(1,self.MAX)
-            priv = PrivateKey(secret_exponent=xx)
-            pub = priv.get_public_key()
-            address = pub.get_address()
-            self.address.append(
-                [address.to_string(), priv.to_wif(), priv.to_bytes()])
-
+        
     def GetAddress2(self):
         setup('mainnet')
         priv = PrivateKey()
@@ -69,9 +57,7 @@ class Lib2(Address):
         x = random.randrange(
             1, self.MAX)
         priv = PrivateKey(secret_exponent=x)
-
         pub = priv.get_public_key()
-
         address = pub.get_address()
         self.address.append(
             [address.to_string(), priv.to_wif(), priv.to_bytes()])
@@ -89,67 +75,25 @@ class Lib3(Address):
     def __init__(self) -> None:
         super().__init__()
         self.address.clear()
-        self.get_Add3()
-        self.get_Add4()
-        self.get_Add5()
-        #self.get_Add1()
-        self.get_Add2()
-        self.get_Add6()
-
+        self.get_Add1()
+        
     def get_Add1(self):
         wallet = Wallet()
-        key = wallet.key.__dict__[self.MAINNET].__dict__[self.WIF]
+        wif = wallet.key.__dict__[self.MAINNET].__dict__[self.WIF]
         hex_ = wallet.key.__dict__[self.HEX]
         adr1 = wallet.address.__dict__[self.MAINNET].__dict__[self.PUBADDR1]
-        self.address.append([adr1, key, hex_])
+        self.address.append([adr1, wif, hex_])
         adr2 = wallet.address.__dict__[self.MAINNET].__dict__[self.PUBADDR1C]
-        self.address.append([adr2, key, hex_])
+        self.address.append([adr2, wif, hex_])
         adr3 = wallet.address.__dict__[self.MAINNET].__dict__[self.PUBADDR3]
-        self.address.append([adr3, key, hex_])
+        self.address.append([adr3, wif, hex_])
         adr4 = wallet.address.__dict__[
             self.MAINNET].__dict__[self.PUBADDRBC1_P2WSH]
-        self.address.append([adr4, key, hex_])
+        self.address.append([adr4, wif, hex_])
         adr5 = wallet.address.__dict__[
             self.MAINNET].__dict__[self.PUBADDRBC1_P2WPKH]
-        self.address.append([adr5, key, hex_])
-
-    def get_Add6(self):
-        wallet = Wallet()
-        adr = wallet.address.__dict__[
-            self.MAINNET].__dict__[self.PUBADDRBC1_P2WPKH]
-        key = wallet.key.__dict__[self.MAINNET].__dict__[self.WIF]
-        hex_ = wallet.key.__dict__[self.HEX]
-        self.address.append([adr, key, hex_])
-
-    def get_Add2(self):
-        wallet = Wallet()
-        adr = wallet.address.__dict__[self.MAINNET].__dict__[self.PUBADDR1C]
-        key = wallet.key.__dict__[self.MAINNET].__dict__[self.WIF]
-        hex_ = wallet.key.__dict__[self.HEX]
-        self.address.append([adr, key, hex_])
-
-    def get_Add3(self):
-        wallet = Wallet()
-        adr = wallet.address.__dict__[
-            self.MAINNET].__dict__[self.PUBADDRBC1_P2WSH]
-        key = (wallet.key.__dict__[self.MAINNET].__dict__[self.WIF])
-        hex_ = wallet.key.__dict__[self.HEX]
-        self.address.append([adr, key, hex_])
-
-    def get_Add4(self):
-        wallet = Wallet()
-        adr = wallet.address.__dict__[self.MAINNET].__dict__[self.PUBADDR3]
-        key = (wallet.key.__dict__[self.MAINNET].__dict__[self.WIF])
-        hex_ = wallet.key.__dict__[self.HEX]
-        self.address.append([adr, key, hex_])
-
-    def get_Add5(self):
-        wallet = Wallet()
-        adr = wallet.address.__dict__[self.MAINNET].__dict__[self.PUBADDR1]
-        key = (wallet.key.__dict__[self.MAINNET].__dict__[self.WIF])
-        hex_ = wallet.key.__dict__[self.HEX]
-        self.address.append([adr, key, hex_])
-
+        self.address.append([adr5, wif, hex_])
+   
 class Lib4(Address):
 
     def __init__(self) -> None:
@@ -162,10 +106,9 @@ class Lib4(Address):
 
     def get_addr1(self):
 
-        i: BIP49HDWallet = BIP49HDWallet()
+        i: BIP32HDWallet = BIP32HDWallet()
         wo = Mnemonic(self.language).generate()
-        h = is_mnemonic(wo)
-        if h:
+        if is_mnemonic(wo):
             i.from_mnemonic(wo)
             self.address.append([i.address(), i.wif(), wo])
             self.address.append([i.p2sh_address(),i.wif(),wo])
@@ -173,34 +116,29 @@ class Lib4(Address):
             self.address.append([i.p2wsh_address(),i.wif(),wo])
             self.address.append([i.p2wpkh_address(),i.wif(),wo])
             self.address.append([i.p2wpkh_in_p2sh_address(),i.wif(),wo])
-            print("hello")
             
     def get_addr2(self):
         try:
-            i: BIP49HDWallet = BIP49HDWallet()
-            m = Mnemonic(self.language)
-            wo = m.generate(strength=128)
-            h = is_mnemonic(wo)
-            if h:
+            
+            i: BIP32HDWallet = BIP32HDWallet()
+            wo = Mnemonic(self.language).generate(strength=128)
+            if is_mnemonic(wo):
                 i.from_mnemonic(wo)
                 self.address.append([i.address(), i.wif(), wo])
-            self.address.append([i.p2sh_address(),i.wif(),wo])
-            self.address.append([i.p2pkh_address(),i.wif(),wo])
-            self.address.append([i.p2wsh_address(),i.wif(),wo])
-            self.address.append([i.p2wpkh_address(),i.wif(),wo])
-            self.address.append([i.p2wpkh_in_p2sh_address(),i.wif(),wo])
+                self.address.append([i.p2sh_address(),i.wif(),wo])
+                self.address.append([i.p2pkh_address(),i.wif(),wo])
+                self.address.append([i.p2wsh_address(),i.wif(),wo])
+                self.address.append([i.p2wpkh_address(),i.wif(),wo])
+                self.address.append([i.p2wpkh_in_p2sh_address(),i.wif(),wo])
         except BaseException as e:
             pass
 
 
 
     def get_addr3(self):
-        i: BIP44HDWallet = BIP44HDWallet()
-
-        m = Mnemonic(self.language)
-        wo = m.generate(strength=256)
-        h = is_mnemonic(mnemonic=wo)
-        if h:
+        i: BIP32HDWallet = BIP32HDWallet()
+        wo = Mnemonic(self.language).generate(strength=256)
+        if is_mnemonic(wo):
             i.from_mnemonic(wo)
             self.address.append([i.address(), i.wif(), wo])
             self.address.append([i.p2sh_address(),i.wif(),wo])
@@ -209,73 +147,8 @@ class Lib4(Address):
             self.address.append([i.p2wpkh_address(),i.wif(),wo])
             self.address.append([i.p2wpkh_in_p2sh_address(),i.wif(),wo])
 
+
 class Lib5(Address):
-    LANGUAGE = 'english'
-    SEGWIT = 'segwit'
-    P2SH_SEGWIT = 'p2sh-segwit'
-    BITCOIN = 'bitcoin'
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.address.clear()
-        self.get_adr1()
-        self.get_adr2()
-        self.get_adr3()
-        #self.get_adr4()
-        self.get_adr5()
-        self.get_adr6()
-
-    def get_adr1(self):
-        name = random.choice(string.ascii_letters)
-        bitcoinlib.wallets.wallet_delete_if_exists(name)
-        p = Mnemonic(language=self.LANGUAGE).generate()
-        
-        w = bitcoinlib.wallets.wallet_create_or_open(name, keys=p, network=self.BITCOIN)
-        key_ = w.get_key()
-        self.address.append([key_.address, key_.wif, p])
-
-    def get_adr2(self):
-        name = random.choice(string.ascii_letters)
-        bitcoinlib.wallets.wallet_delete_if_exists(name)
-        p = Mnemonic(language=self.LANGUAGE).generate()
-        w = bitcoinlib.wallets.wallet_create_or_open(
-            name, keys=p, network=self.BITCOIN, witness_type=self.SEGWIT)
-        key_ = w.get_key()
-        
-        self.address.append([key_.address, key_.wif, p])
-
-    def get_adr3(self):
-        name = random.choice(string.ascii_letters)
-        bitcoinlib.wallets.wallet_delete_if_exists(name)
-        p = Mnemonic(language=self.LANGUAGE).generate()
-        
-        w = bitcoinlib.wallets.wallet_create_or_open(
-            name, keys=p, network=self.BITCOIN, witness_type=self.P2SH_SEGWIT)
-        
-        key_ = w.get_key()
-        self.address.append([key_.address, key_.wif, p])
-
-    def get_adr4(self):
-        name = random.choice(string.ascii_letters)
-        bitcoinlib.wallets.wallet_delete_if_exists(name)
-        w = bitcoinlib.wallets.wallet_create_or_open(
-            name, keys=[Keys.HDKey(), Keys.HDKey().public()], network=self.BITCOIN)
-        key_ = w.get_key()
-        self.address.append([key_.address, key_.wif, "Lib5"])
-
-    def get_adr5(self):
-        name = random.choice(string.ascii_letters)
-        bitcoinlib.wallets.wallet_delete_if_exists(name)
-        w = bitcoinlib.wallets.wallet_create_or_open(name, network=self.BITCOIN)
-        key_ = w.get_key()
-        self.address.append([key_.address, key_.wif, "Lib5"])
-
-    def get_adr6(self):
-        k = Keys.HDKey(multisig=True)
-        self.address.append([k.address(),k.private_byte,k.private_hex])
-  
-
-class Lib6(Address):
     HEX = 'hex'
     X0 = '0x'
     B0 = '0b'
@@ -316,6 +189,7 @@ class Lib6(Address):
             address = pub.get_address(False).to_string()
             c_address = pub.get_address(True).to_string()
             seg_adr = pub.get_segwit_address().to_string()
+            
             self.address.append([address, hex_,wif])
             self.address.append([seg_adr,hex_,wif])
             self.address.append([c_address,hex_,wif])
@@ -328,5 +202,3 @@ class AddressFact():
         targetclass = typ
         return globals()[targetclass]()
 
-
-    
